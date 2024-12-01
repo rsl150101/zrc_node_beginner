fc30691869f9b9c6e708ef7968077e7d88d9e3b9
rsl150101 <dnwnd501@naver.com>
2024-12-01 17:43:06 +0900

    [Add] 10-3 call server

---
+++ ch10_call_server/app.js
@@ -0,0 +1,58 @@
+import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import path from "path";
+import nunjucks from "nunjucks";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+//- router
+import indexRouter from "./routes/index.routes";
+
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", indexRouter);
+
+//- Handle 404
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} Not found router`);
+  error.status = 404;
+  next(error);
+});
+
+//- Handle Error
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening on http://localhost:${app.get("port")}`);
+});
